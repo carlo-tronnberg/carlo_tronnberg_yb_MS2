f2cd4872a91b07dc5487f15a2acb900a68c138c9
Carlo Trönnberg <carlo.tronnberg@hu.ibm.com>
2021-11-29 20:42:53 +0100

    chore: ✨ create dummy files
    Create dummy test and implementation files

---
+++ src/mS2.js
@@ -0,0 +1,17 @@
+class carlo_tronnberg_yb_MS2 {
+  constructor() {
+    this.status = 'running';
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getValue(x) {
+    if (x == 1) return 'one';
+    return 'two';
+  }
+}
+
+module.exports = { carlo_tronnberg_yb_MS2 };
+
---
+++ test/mS2.test.js
@@ -0,0 +1,21 @@
+const { carlo_tronnberg_yb_MS2 } = require('../src/mS2');
+
+
+describe('carlo_tronnberg_yb_MS2', () => {
+  it('Start a new mS2', () => {
+    const mS2 = new carlo_tronnberg_yb_MS2();
+
+    expect(mS2.getStatus()).toEqual('running');
+  });
+});
+
+describe('carlo_tronnberg_yb_MS2 2', () => {
+  it.each([
+    [1, 'one'],
+    [2, 'two'],
+  ])("Given ,      When %i,      Then  '%s'", (x, result) => {
+    const mS2 = new carlo_tronnberg_yb_MS2();
+    expect(mS2.getValue(x)).toEqual(result);
+  });
+});
+
